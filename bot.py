c991c27d26b371225383bf04ac86967169a81e73
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-11-17 09:30:22 +1100

    Update: Added withdrawing and fixed up tipping
    Signed-off-by: Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
 import logging
 import json
 import requests
 import db
+
 from bitcoinutils.setup import setup
-from bitcoinutils.keys import P2wpkhAddress, P2wshAddress, P2shAddress, PrivateKey, PublicKey
+from bitcoinutils.keys import P2wpkhAddress, P2wshAddress, P2shAddress, P2pkhAddress, PrivateKey
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput
-from bitcoinutils.script import Script, OP_CODES
+from bitcoinutils.script import Script
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -31,7 +33,13 @@ def help(update, ctx):
     3\\. /tip @user amount
     4\\. /deposit
     5\\. /balance
+    6\\. /withdraw address amount
             """, parse_mode="MarkdownV2")
+            ctx.bot.send_message(chat_id=update.message.chat_id,
+                                 text="*Please Note: * It is highly recommended that you do not directly mine to the "
+                                      "address given by this bot\\. Download a full node here: "
+                                      "[Full Node](https://github\\.com/sugarchain\\-project/sugarchain/releases/latest)",
+                                 parse_mode="MarkdownV2")
         else:
             ctx.bot.send_message(chat_id=update.message.chat_id, text=f"""
     Hey there [{user['first_name']}](tg://user?id={user['id']})\\. Here are my commands:
@@ -40,7 +48,13 @@ def help(update, ctx):
     3\\. /tip @user amount
     4\\. /deposit
     5\\. /balance
+    6\\. /withdraw address amount
             """, parse_mode="MarkdownV2")
+            ctx.bot.send_message(chat_id=update.message.chat_id,
+                                 text="*Please Note: * It is highly recommended that you do not directly mine to the "
+                                      "address given by this bot\\. Download a full node here: "
+                                      "[Full Node](https://github\\.com/sugarchain\\-project/sugarchain/releases/latest)",
+                                 parse_mode="MarkdownV2")
     else:
         ctx.bot.send_message(chat_id=update.message.chat_id, text=f"[{user['first_name']}](tg://user?id={user['id']}), please set a username before using this bot", parse_mode="MarkdownV2")
 
@@ -53,7 +67,7 @@ def price(update, ctx):
 
     ctx.bot.send_message(chat_id=update.message.chat_id, text=f"Current {config['coin']['ticker']}/BTC price: {btc} BTC\nCurrent {config['coin']['ticker']}/USD price: ${usd}")
 
-def tipSomeone(update, ctx):
+def tip(update, ctx):
     user = update.message.from_user
     args = update.message.text.split(" ")
 
@@ -83,18 +97,20 @@ def tipSomeone(update, ctx):
                 else:
                     if amount is not None:
                         if isFloat(amount):
-                            if float(amount) > 0:
+                            if float(amount) > float(config['coin']['minFee']):
                                 keyboard = [
-                                    [InlineKeyboardButton("Yes", callback_data=f"Y, {target}, {amount}, {user['id']}"),
-                                     InlineKeyboardButton("No", callback_data=f"N, {target}, {amount}, {user['id']}")]
+                                    [
+                                        InlineKeyboardButton("Yes", callback_data=f"Y, {target}, {amount}, {user['id']}, tip"),
+                                        InlineKeyboardButton("No", callback_data=f"N, {target}, {amount}, {user['id']}, tip")
+                                    ]
                                 ]
                                 reply_markup = InlineKeyboardMarkup(keyboard)
                                 ctx.bot.send_message(chat_id=update.message.chat_id,
-                                                     text=f"You are about to send {amount} {config['coin']['ticker']} to @{target}. Please click Yes to confirm",
+                                                     text=f"You are about to send {amount} {config['coin']['ticker']} with an additional fee of {format(config['coin']['minFee'], '.8f')} SUGAR to @{target}. Please click Yes to confirm",
                                                      reply_markup=reply_markup)
                             else:
                                 ctx.bot.send_message(chat_id=update.message.chat_id,
-                                                     text="You cannot send negative amounts!")
+                                                     text="You cannot send negative amounts or amounts less than 0.00001!")
                         else:
                             ctx.bot.send_message(chat_id=update.message.chat_id,
                                                  text="Invalid amount of SUGAR. Please try again")
@@ -102,6 +118,56 @@ def tipSomeone(update, ctx):
                         ctx.bot.send_message(chat_id=update.message.chat_id, text="No amount specified!")
         else:
             ctx.bot.send_message(chat_id=update.message.chat_id, text="No user specified!")
+
+
+def withdraw(update, ctx):
+    user = update.message.from_user
+    args = update.message.text.split(" ")
+
+    if not db.checkUser(user['id']):
+        ctx.bot.send_message(chat_id=update.message.chat_id,
+                             text="It looks like you haven't registered yet. Please run /help first to register yourself")
+    else:
+        address = None
+        try:
+            address = str(args[1])[7:]
+        except IndexError:
+            address = address
+
+        amount = None
+        try:
+            amount = args[2]
+        except IndexError:
+            amount = amount
+
+        if address is not None:
+            if checkAdd("sugar1q" + address):
+                if amount is not None:
+                    if isFloat(amount):
+                        if float(amount) > float(config['coin']['minFee']):
+                            keyboard = [
+                                [
+                                    InlineKeyboardButton("Yes", callback_data=f"Y, {address}, {amount}, {user['id']}, w"),
+                                    InlineKeyboardButton("No", callback_data=f"N, {address}, {amount}, {user['id']}, w")
+                                ]
+                            ]
+                            reply_markup = InlineKeyboardMarkup(keyboard)
+                            ctx.bot.send_message(chat_id=update.message.chat_id,
+                                                 text=f"You are about to withdraw {amount} {config['coin']['ticker']}, with a fee of {format(config['coin']['minFee'], '.8f')} SUGAR to {'sugar1q' + address}. Please click Yes to confirm",
+                                                 reply_markup=reply_markup)
+                        else:
+                            ctx.bot.send_message(chat_id=update.message.chat_id, text="You cannot withdraw negative amounts or amounts less than 0.00001")
+                    else:
+                        ctx.bot.send_message(chat_id=update.message.chat_id, text="The amount you have specified is not valid. Please try again.")
+                else:
+                    ctx.bot.send_message(chat_id=update.message.chat_id, text="You did not specify the amount you wish to withdraw. Please try again")
+            else:
+                ctx.bot.send_message(chat_id=update.message.chat_id, text="You have specified an invalid withdraw address. Try again with a valid address.")
+        else:
+            ctx.bot.send_message(chat_id=update.message.chat_id, text="No withdraw address specified")
+
+
+
 
 
 def deposit(update, ctx):
@@ -145,7 +211,7 @@ def genAddress():
     return wif
 
 
-def tip(update, ctx):
+def tip_or_withdrawFunc(update, ctx):
     # Initialise bitcoin.py
     setup('mainnet')
     query = update.callback_query
@@ -154,23 +220,111 @@ def tip(update, ctx):
     query.answer()
     data = str(query.data).split(", ")
     sender = str(query.from_user.id)
-    target = db.getUserID(data[1])
     if sender == data[3]:
-        if data[0] == "Y":
-            ctx.bot.delete_message(chat_id=chID, message_id=msgID)
-            sender_wif = db.getWIF(sender)
-            target_address = getAddress(target)
-            sender_address = getAddress(sender)
-            sender_balance = getBalance(sender)
-            value = 0
-            amount = convertToSatoshis(float(data[2]))
-            api = requests.get(f"{config['apiUrl']}/unspent/{sender_address}").json()["result"]
-            print(api)
-            print(sender_balance)
-            ctx.bot.send_message(chat_id=chID, text=f"Success, sent @{data[1]} {data[2]} {config['coin']['ticker']}")
-        elif data[0] == "N":
-            ctx.bot.delete_message(chat_id=chID, message_id=msgID)
-            ctx.bot.send_message(chat_id=chID, text=f"You declined sending @{data[1]} {data[2]} {config['coin']['ticker']}")
+        if data[4] == "tip":
+            target = db.getUserID(data[1])
+            if data[0] == "Y":
+                ctx.bot.delete_message(chat_id=chID, message_id=msgID)
+
+                sender_wif = PrivateKey(db.getWIF(sender))
+                fee = convertToSatoshis(float(config['coin']['minFee']))
+                target_address = P2wpkhAddress(getAddress(target))
+                sender_address = P2wpkhAddress(getAddress(sender))
+                sender_balance = 0
+                amount = convertToSatoshis(float(data[2])) + fee
+
+                unspent = requests.get(f"{config['apiUrl']}/unspent/{sender_address.to_string()}").json()["result"]
+                txin = []
+                for i in range(0, len(unspent)):
+                    sender_balance += unspent[i]['value']
+                    txin.append(TxInput(unspent[i]['txid'], unspent[i]['index']))
+
+                if sender_balance >= amount:
+
+                    txout = []
+                    txout.append(TxOutput((amount - fee), target_address.to_script_pub_key()))
+
+                    txchange = sender_balance - amount
+                    if txchange > 0:
+                        txout.append(TxOutput(txchange, sender_address.to_script_pub_key()))
+
+                    script_code = Script(['OP_DUP', 'OP_HASH160', sender_wif.get_public_key().to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+
+                    tx = Transaction(txin, txout, has_segwit=True)
+
+                    tx.witnesses = []
+
+                    for i in range(0, len(unspent)):
+                        value = unspent[i]['value']
+                        sig = sender_wif.sign_segwit_input(tx, i, script_code, value)
+                        tx.witnesses.append(Script([sig, sender_wif.get_public_key().to_hex()]))
+
+                    post_data = {
+                        'raw': tx.serialize()
+                    }
+
+                    txid = requests.post(f"{config['apiUrl']}/broadcast", data=post_data).json()['result']
+
+                    ctx.bot.send_message(chat_id=chID, text=f"Success, sent @{data[1]} {data[2]} {config['coin']['ticker']}.")
+                    ctx.bot.send_message(chat_id=chID, text=f"[View Transaction](https://sugar\\.wtf/esplora/tx/{str(txid)})", parse_mode="MarkdownV2")
+                else:
+                    ctx.bot.send_message(chat_id=chID, text="You do not have enough funds to tip that amount")
+
+            elif data[0] == "N":
+                ctx.bot.delete_message(chat_id=chID, message_id=msgID)
+                ctx.bot.send_message(chat_id=chID, text=f"You declined sending @{data[1]} {data[2]} {config['coin']['ticker']}")
+
+        elif data[4] == "w":
+            if data[0] == "Y":
+                ctx.bot.delete_message(chat_id=chID, message_id=msgID)
+
+                sender_wif = PrivateKey(db.getWIF(sender))
+                fee = convertToSatoshis(float(config['coin']['minFee']))
+                sender_address = P2wpkhAddress(getAddress(sender))
+                sender_balance = 0
+                amount = convertToSatoshis(float(data[2])) + fee
+                target_address = P2wpkhAddress("sugar1q" + data[1])
+
+                unspent = requests.get(f"{config['apiUrl']}/unspent/{sender_address.to_string()}").json()['result']
+
+                txin = []
+                for i in range(0, len(unspent)):
+                    sender_balance += unspent[i]['value']
+                    txin.append(TxInput(unspent[i]['txid'], unspent[i]['index']))
+
+                if sender_balance >= amount:
+                    txout = []
+                    
+                    txout.append(TxOutput((amount - fee), target_address.to_script_pub_key()))
+
+                    txchange = sender_balance - amount
+                    if txchange > 0:
+                        txout.append(TxOutput(txchange, sender_address.to_script_pub_key()))
+
+                    script_code = Script(['OP_DUP', 'OP_HASH160', sender_wif.get_public_key().to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+
+                    tx = Transaction(txin, txout, has_segwit=True)
+
+                    tx.witnesses = []
+
+                    for i in range(0, len(unspent)):
+                        value = unspent[i]['value']
+                        sig = sender_wif.sign_segwit_input(tx, i, script_code, value)
+                        tx.witnesses.append(Script([sig, sender_wif.get_public_key().to_hex()]))
+
+                    post_data = {
+                        'raw': tx.serialize()
+                    }
+
+                    txid = requests.post(f"{config['apiUrl']}/broadcast", data=post_data).json()['result']
+
+                    ctx.bot.send_message(chat_id=chID, text=f"Success, withdrew {data[2]} {config['coin']['ticker']} to address {target_address.to_string()} ")
+                    ctx.bot.send_message(chat_id=chID, text=f"[View Transaction](https://sugar\\.wtf/esplora/tx/{str(txid)})", parse_mode="MarkdownV2")
+                else:
+                    ctx.bot.send_message(chat_id=chID, text="You do not have enough funds to withdraw the specified amount.")
+            elif data[0] == "N":
+                ctx.bot.delete_message(chat_id=chID, message_id=msgID)
+                ctx.bot.send_message(chat_id=chID, text=f"You declined withdrawing {data[2]} {config['coin']['ticker']} to address {data[1]}")
 
 
 def getBalance(id: str):
@@ -181,6 +335,15 @@ def getBalance(id: str):
     userBalance = getBalance / 100000000
 
     return userBalance
+
+
+def checkAdd(address: str):
+    check = requests.get(f"{config['apiUrl']}/balance/{address}").json()
+
+    if check['error']:
+        return False
+    else:
+        return True
 
 
 def getAddress(id: str):
@@ -197,8 +360,10 @@ def getAddress(id: str):
     return address
 
 def convertToSatoshis(amount: float):
-    return amount * 100000000
+    return int(round(amount * 100000000))
 
+def convertToSugar(amount: int):
+    return float(round(amount / 100000000))
 
 ### LAUNCH
 
@@ -208,18 +373,21 @@ def main():
 
     dispatcher = updater.dispatcher
 
-    help_handler = CommandHandler('help', help)
-    price_handler = CommandHandler('price', price)
-    tip_command = CommandHandler('tip', tipSomeone)
-    deposit_handler = CommandHandler('deposit', deposit)
-    balance_handler = CommandHandler('balance', balance)
-    tip_handler = CallbackQueryHandler(tip)
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(price_handler)
+    help_command = CommandHandler('help', help)
+    price_command = CommandHandler('price', price)
+    tip_command = CommandHandler('tip', tip)
+    deposit_command = CommandHandler('deposit', deposit)
+    balance_command = CommandHandler('balance', balance)
+    withdraw_command = CommandHandler('withdraw', withdraw)
+    tip_or_withdraw_handler = CallbackQueryHandler(tip_or_withdrawFunc)
+    dispatcher.add_handler(help_command)
+    dispatcher.add_handler(price_command)
     dispatcher.add_handler(tip_command)
-    dispatcher.add_handler(tip_handler)
-    dispatcher.add_handler(deposit_handler)
-    dispatcher.add_handler(balance_handler)
+    dispatcher.add_handler(deposit_command)
+    dispatcher.add_handler(balance_command)
+    dispatcher.add_handler(withdraw_command)
+
+    dispatcher.add_handler(tip_or_withdraw_handler)
 
     updater.start_polling()
     updater.idle()
